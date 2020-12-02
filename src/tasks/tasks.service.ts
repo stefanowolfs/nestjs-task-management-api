88386126ec76b38f21fa05b44a954225b7b31c45
Stefano Soro <stefanowolfs@gmail.com>
2020-12-02 10:38:16 -0300

    :sparkles: Add update and get with filters methods to tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuidv1 } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Injectable()
 export class TasksService {
@@ -11,6 +12,18 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTasksWithFilters(filterDto: GetTasksFilterDto) {
+    const { status, search } = filterDto;
+    let tasks: Task[] = this.getAllTasks();
+    if (status) tasks = tasks.filter((task) => task.status === status);
+    if (search) {
+      tasks = tasks.filter((task) => {
+        return task.title.includes(search) || task.description.includes(search);
+      });
+    }
+    return tasks;
+  }
+
   getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
   }
@@ -18,17 +31,23 @@ export class TasksService {
   createTask(createTaskDto: CreateTaskDto): Task {
     const { title, description } = createTaskDto;
 
-    const Task: Task = {
+    const task: Task = {
       id: uuidv1(),
       title,
       description,
       status: TaskStatus.OPEN,
     };
-    this.tasks.push(Task);
-    return Task;
+    this.tasks.push(task);
+    return task;
   }
 
   deleteTask(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
   }
 }
