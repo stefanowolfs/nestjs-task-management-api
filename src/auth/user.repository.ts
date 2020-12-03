1f90dd856713bc0124cb813b74d0b0def413666a
Stefano Soro <stefanowolfs@gmail.com>
2020-12-02 21:23:20 -0300

    :sparkles: Add auth context to application

---
+++ src/auth/user.repository.ts
@@ -0,0 +1,28 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exists');
+      }
+      throw new InternalServerErrorException();
+    }
+  }
+}
