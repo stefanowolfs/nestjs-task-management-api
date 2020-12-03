de65030e96322e88622dd52aa0936647a53d8a07
Stefano Soro <stefanowolfs@gmail.com>
2020-12-02 23:51:44 -0300

    :sparkles: Add token generation on signIn endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,32 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { JwtPayload } from './jwt-payload.interface';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return await this.userRepository.signUp(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto) {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ acessToken: string }> {
     const username = await this.userRepository.validateUserPassword(
       authCredentialsDto,
     );
-    console.log(username);
+
     if (!username) throw new UnauthorizedException('Invalid credentials');
+
+    const payload: JwtPayload = { username };
+    const acessToken = await this.jwtService.sign(payload);
+    return { acessToken };
   }
 }
