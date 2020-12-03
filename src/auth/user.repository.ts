26b4a8984ed10492db4fb34a623d0608ff0bb526
Stefano Soro <stefanowolfs@gmail.com>
2020-12-02 21:43:09 -0300

    :sparkles: Add random salt per user and cryptografhy

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,6 +5,7 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -13,7 +14,9 @@ export class UserRepository extends Repository<User> {
 
     const user = new User();
     user.username = username;
-    user.password = password;
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+    console.log(user.password);
 
     try {
       await user.save();
@@ -24,5 +27,9 @@ export class UserRepository extends Repository<User> {
       }
       throw new InternalServerErrorException();
     }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
   }
 }
