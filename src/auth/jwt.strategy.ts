1e288ec7f97ca7e97a3e0c12bd9f5cfbebf6a44d
stefanowolfs <stefanowolfs@gmail.com.br>
2020-12-06 19:46:04 -0300

    :recycle: Code refactoring

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,9 +8,7 @@ import { UserRepository } from './user.repository';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    @InjectRepository(UserRepository) private userRepository: UserRepository,
-  ) {
+  constructor(@InjectRepository(UserRepository) private userRepository: UserRepository) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: '@myCustom_TopçÇ$Secret2020!',
