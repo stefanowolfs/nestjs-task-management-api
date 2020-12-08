c910b2973dd414048d69746c01550f0d7db19e0a
stefanowolfs <stefanowolfs@gmail.com.br>
2020-12-07 22:47:11 -0300

    :white_check_mark: Add unit test to tasksService getTasks method

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,6 +1,12 @@
 import { Test } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
 import { TaskRepository } from './task.repository';
+import { TaskStatus } from './tasks-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { Task } from './task.entity';
+
+// since we are going to use this same user a lot, I declared it here
+const mockUser = { username: 'Test User' };
 
 // It's a good practice to mock only the methods that you're going to use.
 const mockTaskRepository = () => ({
@@ -11,14 +17,14 @@ describe('tasksService', () => {
   let tasksService;
   let taskRepository;
 
+  // re-initialize service for each test...
   beforeEach(async () => {
-    // re-initialize service for each test...
-    // ...but not the database connection since it is costly and needless to.
+    // Create and 'compile' a new module for testing.
     const module = await Test.createTestingModule({
       providers: [
         TasksService,
-        // TaskRepository, // I'm not using this since it brings the whole repository and it's methods
-        { provide: TaskRepository, useFactory: mockTaskRepository },
+        // TaskRepository, // I'm not using this since it brings the whole repository and it's methods.
+        { provide: TaskRepository, useFactory: mockTaskRepository }, // 'useFactory' re-creates the mock every time.
       ],
     }).compile();
 
@@ -27,8 +33,15 @@ describe('tasksService', () => {
   });
 
   describe('getTasks', () => {
-    it('gets all tasks from the repository', () => {
+    it('gets all tasks from the repository', async () => {
+      const expectedResult = Array.of(new Task());
+      taskRepository.getTasks.mockResolvedValue(expectedResult);
+
       expect(taskRepository.getTasks).not.toHaveBeenCalled();
+      const filters: GetTasksFilterDto = { status: TaskStatus.IN_PROGRESS, search: 'Some search query' };
+      const result = await tasksService.getTasks(filters, mockUser);
+      expect(taskRepository.getTasks).toHaveBeenCalled();
+      expect(result).toEqual(expectedResult);
     });
   });
 });
