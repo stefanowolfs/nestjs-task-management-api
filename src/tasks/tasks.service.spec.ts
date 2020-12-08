9329219e9a1c035d0f5d6e454602c971ec648ae3
stefanowolfs <stefanowolfs@gmail.com.br>
2020-12-07 23:01:36 -0300

    :white_check_mark: Add unit test to tasksService getTaskById method

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -4,13 +4,17 @@ import { TaskRepository } from './task.repository';
 import { TaskStatus } from './tasks-status.enum';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { Task } from './task.entity';
+import { User } from '../auth/user.entity';
 
 // since we are going to use this same user a lot, I declared it here
-const mockUser = { username: 'Test User' };
+const mockUser = new User();
+mockUser.id = 45;
+mockUser.username = 'Test User';
 
 // It's a good practice to mock only the methods that you're going to use.
 const mockTaskRepository = () => ({
   getTasks: jest.fn(),
+  findOne: jest.fn(),
 });
 
 describe('tasksService', () => {
@@ -42,6 +46,23 @@ describe('tasksService', () => {
       const result = await tasksService.getTasks(filters, mockUser);
       expect(taskRepository.getTasks).toHaveBeenCalled();
       expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('getTaskById', () => {
+    it('calls taskRepository.findOne() and successfully retrieve and return the task', async () => {
+      const expectedResult = { title: 'Test task', description: 'Test desc' };
+      const mockNumberToSatisfyParameters = 1;
+      taskRepository.findOne.mockResolvedValue(expectedResult);
+
+      const result = await tasksService.getTaskById(mockNumberToSatisfyParameters, mockUser);
+      expect(result).toEqual(expectedResult);
+      expect(taskRepository.findOne).toHaveBeenCalledWith({
+        where: {
+          id: 1,
+          userId: mockUser.id,
+        },
+      });
     });
   });
 });
